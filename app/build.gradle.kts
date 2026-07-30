10b02ec486cf038211b81bc5e59dc8b5fc847986
hadi892 <amerianh3@gmail.com>
2026-07-30 20:52:52 +0330

    build: disable lint checks for build process
    Configure lint to not abort on error and skip release build checks to streamline CI/CD and local development workflows.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,6 +79,10 @@ android {
     buildConfig = true
   }
   testOptions { unitTests { isIncludeAndroidResources = true } }
+  lint {
+    abortOnError = false
+    checkReleaseBuilds = false
+  }
 }
 
 // Configure the Secrets Gradle Plugin to use .env and .env.example files
